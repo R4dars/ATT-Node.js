0674d96f1b62fd96884ddf38bec3c7aa54976a2e
Maatheuss <111097490+R4dars@users.noreply.github.com>
2023-03-06 11:45:21 -0300

    Exercise Tower-of-Babel

---
+++ tower-of-babel-ATT/babel-setup.js
@@ -0,0 +1,7 @@
+//Necessário instalar os módulos Babel, utilize as 2 opções abaixo:
+// npm install babel-cli -g
+// npm install --save-dev @babel/preset-env
+
+var a = 'Babel';
+console.log(`Hello ${a}`);
+
